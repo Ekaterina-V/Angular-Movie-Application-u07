86a194d5ba5266f38b2de94670672130deeaa94c
Ekaterina Voronina <ekaterina.voronina@chasacademy.se>
2020-07-02 15:41:32 +0200

    Add components and app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopRatedMoviesListComponent } from './top-rated-movies-list/top-rated-movies-list.component';
+import { HomeComponent } from './home/home.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { ActorDetailComponent } from './actor-detail/actor-detail.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopRatedMoviesListComponent,
+    HomeComponent,
+    MovieDetailComponent,
+    ActorDetailComponent,
+    SearchFormComponent,
+    SearchResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-rated-movies-list/top-rated-movies-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-rated-movies-list',
+  templateUrl: './top-rated-movies-list.component.html',
+  styleUrls: ['./top-rated-movies-list.component.css']
+})
+export class TopRatedMoviesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
